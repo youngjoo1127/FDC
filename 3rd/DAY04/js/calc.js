ff4673f59f23a3441b7f511d85116abb2ebec8ff
Jee Hoon <yamoo9@naver.com>
2016-10-14 00:54:29 +0900

    함수 리팩토링

--- 3rd/DAY04/js/calc.js
+++ 3rd/DAY04/js/calc.js
@@ -1,19 +1,53 @@
-function marginBottom(x) {
-    return 1.5/x;
+/**
+ *  @function marginBottom
+ *  @author   yamoo9
+ *  @version  1.0.0
+ *  @param    {number}  x
+ *  @param    {number}  ratio
+ *  @return   {number}  글자 크기(상대 값)에 상대적인 margin-bottom 비율
+ */
+function marginBottom(x, ratio) {
+    ratio = ratio || 1.5;
+    return ratio/x;
 }
 
-function lineHeight(x){
-   return Math.ceil(x/1.5) * marginBottom(x);
+/**
+ *  @function lineHeight
+ *  @author   yamoo9
+ *  @version  1.0.0
+ *  @param    {number}  x
+ *  @param    {number}  ratio
+ *  @return   {number}  글자 크기(상대 값)에 상대적인 행간 비율
+ */
+function lineHeight(x, ratio) {
+    ratio = ratio || 1.5;
+   return Math.ceil(x/ratio) * marginBottom(x, ratio);
 }
 
-function fontSize(x, ratio) {
-    ratio = ratio || 1.25;
-    var headings = {};
-    headings.h6 = x * ratio;
-    headings.h5 = x * ratio * ratio;
-    headings.h4 = x * ratio * ratio * ratio;
-    headings.h3 = x * ratio * ratio * ratio * ratio;
-    headings.h2 = x * ratio * ratio * ratio * ratio * ratio;
-    headings.h1 = x * ratio * ratio * ratio * ratio * ratio * ratio;
-    return headings;
-}
+/**
+ *  @function fontSize
+ *  @author   yamoo9
+ *  @version  1.0.0
+ *  @param    {number}  type_scale
+ *  @param    {number}  limit
+ *  @return   {object}  제목 글자 크기 상대 값을 반환하는 객체
+ */
+var fontSize = (function() {
+    var square = (x, count) => {
+        var y = x;
+        while(--count) { x *= y; }
+        return x;
+    };
+    return (type_scale, limit) => {
+        type_scale = type_scale || 1.25;
+        limit = limit || 6;
+        var headings_map = {};
+        var headings = 'h1 h2 h3 h4 h5 h6'.split(' ');
+        headings = headings.slice(0, limit);
+        console.log(headings.length);
+        headings.forEach(function(heading, index) {
+            headings_map[heading] = square(type_scale, headings.length - index);
+        });
+        return headings_map;
+    };
+})();
