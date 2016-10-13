5c44bcbd7ebdd812e6a375affd08dbdbdd98b1c0
Jee Hoon <yamoo9@naver.com>
2016-10-13 20:50:10 +0900

    fontSize 함수 추가

--- 3rd/DAY04/js/calc.js
+++ 3rd/DAY04/js/calc.js
@@ -5,3 +5,15 @@ function marginBottom(x) {
 function lineHeight(x){
    return Math.ceil(x/1.5) * marginBottom(x);
 }
+
+function fontSize(x, ratio) {
+    ratio = ratio || 1.25;
+    var headings = {};
+    headings.h6 = x * ratio;
+    headings.h5 = x * ratio * ratio;
+    headings.h4 = x * ratio * ratio * ratio;
+    headings.h3 = x * ratio * ratio * ratio * ratio;
+    headings.h2 = x * ratio * ratio * ratio * ratio * ratio;
+    headings.h1 = x * ratio * ratio * ratio * ratio * ratio * ratio;
+    return headings;
+}
